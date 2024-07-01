d23ca03cac3a3d426da389866f7b2138755be416
aditya-v0921 <adityagv30@gmail.com>
2024-06-30 17:31:37 -0700

    Initial Commit

---
+++ EasyTraffic/EasyTrafficApp.swift
@@ -0,0 +1,17 @@
+//
+//  EasyTrafficApp.swift
+//  EasyTraffic
+//
+//  Created by Aditya Vaswani on 6/30/24.
+//
+
+import SwiftUI
+
+@main
+struct EasyTrafficApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
